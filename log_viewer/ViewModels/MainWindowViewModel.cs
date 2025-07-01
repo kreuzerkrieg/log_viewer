5ef477dd562e55419c6a6c460e823486176d8e46
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-07-01 20:56:40 +0300

    wip

---
+++ log_viewer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace log_viewer.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ log_viewer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace log_viewer.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
